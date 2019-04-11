a005e451f304dfac3b7e70771b952592fce787e7
liuchunliang <Chunliang.Liu@abcam.com>
2019-04-11 12:46:27 +0800

    多个实例使用不同配置

--- lib/xiaodao_mega_lotto/drawing.rb
+++ lib/xiaodao_mega_lotto/drawing.rb
@@ -2,16 +2,17 @@ module XiaodaoMegaLotto
   class Drawing
     attr_accessor :config
 
-    # def initialize(config = Configuration.new)
-    #   @config = config
-    # end
+    def initialize(config = Configuration.new)
+      @config = config
+    end
 
     def draw
-      Configuration.new.drawing_count.times.map { single_draw }
+      @config.drawing_count.times.map { single_draw }
     end
+
     private
       def single_draw
-          rand(0...60)
+        rand(0...60)
       end
   end
 end
